116d6571188bb8d64be6fcc78c5bb08b660da612
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-06-14 14:40:22 +0700

    Fix issue on absolute text top of image

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -95,14 +95,12 @@ const HomePage = () => {
             key={item.id}
             class="max-w-sm bg-white border border-gray-200 rounded-xl drop-shadow-md"
           >
-            <img
-              class="rounded-t-lg relative"
-              src="/SmartSpark8-Webinar.jpg"
-              alt=""
-            />
-            <p className="text-[10px] bg-primary-yellow absolute top-2 left-2 rounded-full px-1 py-0.5 font-semibold">
-              {item.status}
-            </p>
+            <div class="relative">
+              <img class="rounded-t-lg" src="/SmartSpark8-Webinar.jpg" alt="" />
+              <p class="text-[10px] bg-primary-yellow absolute top-2 left-2 rounded-full px-1 py-0.5 font-semibold">
+                {item.status}
+              </p>
+            </div>
 
             <div class="p-5">
               <h5 class="mb-2 text-2xl text-center font-bold">
