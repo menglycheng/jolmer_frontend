9dc7e661ee9fa17ff7e3f6dc5f4e6fb809cab0a4
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-06-29 17:03:56 +0700

    Fetch data form api

--- src/components/Homepage/Card.jsx
+++ src/components/Homepage/Card.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Card = ({ data }) => {
   return (
-    <div className="grid grid-flow-row md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-6  place-content-center mt-20">
+    <div className="grid grid-flow-row md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-6 place-content-center mt-20">
       {data.map((item) => (
         <Link
           key={item.id}
@@ -16,30 +16,28 @@ const Card = ({ data }) => {
             <Image
               width={300}
               height={300}
-              class="rounded-t-lg w-full"
-              src="/SmartSpark8-Webinar.jpg"
-              alt=""
+              class="rounded-t-lg w-full bg-black h-52 object-cover"
+              src={"/" + item.poster}
+              alt="Poster"
             />
             <p class="text-[10px] bg-primary-yellow absolute top-2 left-2 rounded-full px-1 py-0.5 font-semibold">
-              {item.status}
+              {item.category}
             </p>
           </div>
 
           <div class="p-5">
-            <h5 class="mb-2 text-2xl text-center font-bold">
-              {item.Orgainize}
-            </h5>
+            <h5 class="mb-2 text-2xl text-center font-bold">{item.title}</h5>
             <p class="mb-3 font-normal text-center text-gray-700">
-              {item.Date}
+              {item.deadline}
             </p>
-            <div class="flex items-center text-sm justify-between">
+            <div class="flex items-center text-sm justify-between space-x-2">
               <div className="flex items-center space-x-1">
                 <MapPinIcon className="icon" />
                 <p>{item.location}</p>
               </div>
               <div className="flex items-center space-x-1">
                 <EyeIcon className="icon" />
-                <p>{item.view}</p>
+                <p>{item.views}</p>
               </div>
             </div>
           </div>
--- src/pages/create-event.jsx
+++ src/pages/create-event.jsx
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import EditDescription from "@/components/EditDescription";
+import { postEvent } from "./api/Event";
 
 const createEvent = () => {
   const EventForm = {
     event_name: "",
     location: "",
-    categorize: "",
+    categorize: "COMPETITION",
     deadline: "",
-    time: "",
-    file: null,
+    registerUrl: "",
+    img: "",
     description: "",
   };
   const [formEvent, setFormEvent] = useState(EventForm);
+  const [formErrors, setFormErrors] = useState({});
 
   const handleChange = (event) => {
-    const { name, value, files } = event.target;
+    const { name, value } = event.target;
     setFormEvent((prevFormEvent) => ({
       ...prevFormEvent,
-      [name]: files ? files[0] : value,
+      [name]: value,
     }));
+
+    // Validate the input and update the formErrors state
+    if (value.trim() === "") {
+      setFormErrors((prevFormErrors) => ({
+        ...prevFormErrors,
+        [name]: "This field is required.",
+      }));
+    } else {
+      setFormErrors((prevFormErrors) => ({
+        ...prevFormErrors,
+        [name]: undefined,
+      }));
+    }
   };
 
   const handleDescriptionChange = (value) => {
@@ -29,8 +44,66 @@ const createEvent = () => {
     }));
   };
 
-  const handleSave = () => {
-    console.log("Form data:", formEvent);
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const imageURL = URL.createObjectURL(file);
+    setFormEvent((prevFormEvent) => ({
+      ...prevFormEvent,
+      img: imageURL,
+    }));
+  };
+
+  const handleSave = async () => {
+    // Perform form validation
+    const errors = {};
+
+    if (!formEvent.event_name) {
+      errors.event_name = "Event Name is required";
+    }
+
+    if (!formEvent.location) {
+      errors.location = "Location is required";
+    }
+
+    if (!formEvent.deadline) {
+      errors.deadline = "Deadline is required";
+    }
+
+    if (!formEvent.registerUrl) {
+      errors.registerUrl = "registerUrl is required";
+    }
+
+    // Check if there are any errors
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return; // Stop form submission if there are errors
+    }
+
+    // If there are no errors, continue with form submission
+
+    const eventData = {
+      ...formEvent,
+      title: formEvent.event_name, // Change event_name to title
+      category: formEvent.categorize, // Change categorize to category
+      poster: formEvent.img,
+      registerUrl: formEvent.registerUrl,
+      deadline: formEvent.deadline,
+      description: formEvent.description,
+      location: formEvent.location,
+    };
+
+    delete eventData.event_name;
+    delete eventData.categorize;
+    delete eventData.img;
+
+    try {
+      const response = await postEvent(eventData);
+      console.log("Event created:", response);
+      // Reset the form after successful creation
+      setFormEvent(EventForm);
+    } catch (error) {
+      console.error("Error creating event:", error);
+    }
   };
 
   const handleCancel = () => {
@@ -66,6 +139,11 @@ const createEvent = () => {
                       onChange={handleChange}
                     />
                   </div>
+                  {formErrors.event_name && (
+                    <p className="text-red-500 text-xs">
+                      {formErrors.event_name}
+                    </p>
+                  )}
                 </div>
                 <div className="grid grid-cols-2 gap-4 md:gap-8">
                   <div className="space-y-2">
@@ -80,19 +158,24 @@ const createEvent = () => {
                         onChange={handleChange}
                       />
                     </div>
+                    {formErrors.location && (
+                      <p className="text-red-500 text-xs">
+                        {formErrors.location}
+                      </p>
+                    )}
                   </div>
                   <div className="space-y-2">
                     <label htmlFor="categorize">Categorize</label>
                     <div>
                       <select
-                        className="w-full rounded-sm font-light text-sm md:text-base focus:bg-transparent  bg-primary-input shadow px-2 py-1"
+                        className="w-full rounded-sm font-light text-sm md:text-base focus:bg-transparent bg-primary-input shadow px-2 py-1"
                         id="categorize"
                         name="categorize"
                         value={formEvent.categorize}
                         onChange={handleChange}
                       >
-                        <option value="Competition">Competition</option>
-                        <option value="Volunteer">Volunteer</option>
+                        <option value="COMPETITION">Competition</option>
+                        <option value="VOLUNTEER">Volunteer</option>
                       </select>
                     </div>
                   </div>
@@ -110,19 +193,29 @@ const createEvent = () => {
                         onChange={handleChange}
                       />
                     </div>
+                    {formErrors.deadline && (
+                      <p className="text-red-500 text-xs">
+                        {formErrors.deadline}
+                      </p>
+                    )}
                   </div>
                   <div className="space-y-2">
-                    <label htmlFor="time">Time</label>
+                    <label htmlFor="time">registerUrl</label>
                     <div>
                       <input
                         className="w-full rounded-sm font-light text-sm md:text-base focus:bg-transparent  bg-primary-input shadow px-2 py-1"
                         type="text"
-                        id="time"
-                        name="time"
-                        value={formEvent.time}
+                        id="registerUrl"
+                        name="registerUrl"
+                        value={formEvent.registerUrl}
                         onChange={handleChange}
                       />
                     </div>
+                    {formErrors.registerUrl && (
+                      <p className="text-red-500 text-xs">
+                        {formErrors.registerUrl}
+                      </p>
+                    )}
                   </div>
                 </div>
               </div>
@@ -140,7 +233,7 @@ const createEvent = () => {
                       type="file"
                       className="hidden"
                       name="file"
-                      onChange={handleChange}
+                      onChange={handleImageUpload}
                     />
                   </label>
                 </div>
