27061c3cb2f8b49e70bc3433d8279641538aad42
menglycheng <menglycheng2@gmail.com>
2023-07-20 22:53:10 +0700

    add footer and donation page

--- src/pages/donation.jsx
+++ src/pages/donation.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import ABA from "../../public/image/ABA.jpg";
-
 const donation = () => {
   return (
     <div className="flex flex-col justify-center items-center">
@@ -9,11 +7,16 @@ const donation = () => {
           Donation
         </h2>
         <p className="mt-4 text-lg leading-8 text-gray-600">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid dignissimos aspernatur iure nihil. Architecto sit, ipsum tempore accusantium, consectetur pariatur dolor tempora commodi quae voluptate necessitatibus perferendis dolores velit molestias.
-          Culpa eos iste nobis cupiditate sit a minima quia. Quas quae corrupti ad excepturi molestiae libero illo debitis modi hic ut quidem cumque, fugit sapiente, tempora mollitia eveniet sunt aliquid.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+          dignissimos aspernatur iure nihil. Architecto sit, ipsum tempore
+          accusantium, consectetur pariatur dolor tempora commodi quae voluptate
+          necessitatibus perferendis dolores velit molestias. Culpa eos iste
+          nobis cupiditate sit a minima quia. Quas quae corrupti ad excepturi
+          molestiae libero illo debitis modi hic ut quidem cumque, fugit
+          sapiente, tempora mollitia eveniet sunt aliquid.
         </p>
       </div>
-      <img className="w-1/2" src={ABA} alt="ABA Logo" />
+      <img className="w-1/2" src="/image/ABA.jpg" alt="ABA Logo" />
     </div>
   );
 };
