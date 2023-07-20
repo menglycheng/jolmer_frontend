dea4bc7574e01e6bf389e7f369a8ad8ec45ae8eb
menglycheng <menglycheng2@gmail.com>
2023-07-20 23:01:20 +0700

    add footer and donation page

--- src/pages/donation.jsx
+++ src/pages/donation.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+
+import Head from "next/head";
 const donation = () => {
   return (
     <div className="flex flex-col justify-center items-center">
--- src/pages/meet-our-team.jsx
+++ src/pages/meet-our-team.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Head from "next/head";
 import { People } from "../../utils/Team";
 const meetOurTeam = () => {
   console.log(People);
