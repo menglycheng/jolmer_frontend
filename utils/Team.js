6cacf83b99a1edf92af0628603f86a99648e3945
menglycheng <menglycheng2@gmail.com>
2023-07-20 22:19:22 +0700

    add meet our team page

---
+++ utils/Team.js
@@ -0,0 +1,46 @@
+import cheng_mengly from "../public/image/cheng_mengly.png";
+import soth_sambath from "../public/image/soth_sambath.png";
+import tek_zanith from "../public/image/tek_zanith.png";
+import yean_sovanvathana from "../public/image/yean_sovanvathana.png";
+import bunchhean from "../public/image/bunchhean.jpg";
+import neath from "../public/image/neath.jpg";
+import senghong from "../public/image/senghong.jpg";
+
+export const People = [
+  {
+    name: "CHENG MENGLY",
+    role: "Project Lead",
+    imageUrl: cheng_mengly,
+  },
+  {
+    name: "SOTH SOMBATH",
+    role: "Backend Lead",
+    imageUrl: soth_sambath,
+  },
+  {
+    name: "YEAN SOVANVATHANA",
+    role: "Frontend Lead",
+    imageUrl: yean_sovanvathana,
+  },
+
+  {
+    name: "CHHIV BUNCHHEAN",
+    role: "Frontend Developer",
+    imageUrl: bunchhean,
+  },
+  {
+    name: "REASEY SEREI NEATH",
+    role: "Frontend Developer",
+    imageUrl: neath,
+  },
+  {
+    name: "TEK ZANITH",
+    role: "Frontend Developer",
+    imageUrl: tek_zanith,
+  },
+  {
+    name: "HORN SENGHONG",
+    role: "Backend Developer",
+    imageUrl: senghong,
+  },
+];
